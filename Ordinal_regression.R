cb469b092ce6433ffca2110cb9e8c9110aa95d81
MagnusbGit <magnus.barmoen@inn.no>
2019-10-09 16:30:50 +0200

    change variables to factors

--- Ordinal_regression.R
+++ Ordinal_regression.R
@@ -7,11 +7,27 @@ table(mydata$q4_10)
 str(mydata$q4_10)
 mydata$q4_10 <- as.ordered(mydata$q4_10)
 mydata$Kjønn <- as.factor(mydata$Kjønn)
-mydata$q3_1b <- as.ordered(mydata$q3_1b) # obs! 4 betyr vet ikke. Skal den fjernes/endres? hvordan?
-
+mydata$q3_1b <- as.ordered(mydata$q3_1b) # obs! 4 betyr vet ikke. Skal den fjernes/endres? hvordan? OG kan vi bruke rank-deficient variables? får warning message. 
 str(mydata$q3_1b)
+mydata$q3_1b[mydata$q3_1b==4] <- NA # does this work? 
+is.na(mydata$q3_1b)
+str(mydata$q2_6)
+
+table(mydata$q2_6.5)
+table(mydata$q2_7.5)  
+str(mydata$q2_6.5)
+str(mydata$q2_7.5)
+mydata$q2_6.5 <- as.factor(mydata$q2_6.5)
+mydata$q2_7.5 <- as.factor(mydata$q2_7.5)
+mydata$q2_10 <- as.ordered(mydata$q2_10)
+
+
 
 # do this with all variables of interest. Maybe extracting interesting variables first? 
+# running model with more variables 
+#variable <- c("Folkemengde", "BefTetthetKommune","q2_6","q2_7","q2_10","Sum.felt.hjortedyr","SausluppetFylke","Tapt.saulam.fylke","Saulamtapprosent.F","Felt.HjortElg.K", "Bearzone","Wolfzone","Wolverinezone", "Lynxzone","AntallRartZone", "bjørn","gaupe","jerv","ulv","Antallarter","ArtTilstede")
+# spørsmål om en rekke variabler. 
+
 
 # Partition data - ser at man gjør dette, og forstår hvorfor, men kan vi gjøre det her? 
 # hvordan blir det f.eks. når man har 5 fra hver kommune og tar bare 80 % ut uten å ta hensy til dette? 
@@ -21,12 +37,14 @@ test <- mydata[ind==2,]
 
 # The model - MASS
 library(MASS)
-m1 <- polr(q4_10~Alder+Kjønn, train, Hess=TRUE)
+m1 <- polr(q4_10~ Alder + Kjønn + q3_1b + q2_6.5 + q2_7.5 + q2_10 + Folkemengde + BefTetthetKommune + Sum.felt.hjortedyr + SausluppetFylke + AntallRartZone + Wolfzone + Antallarter + ArtTilstede, train, Hess=TRUE)
+str(mydata$Wolfzone)
+mydata$Wolfzone <- as.factor (mydata$Wolfzone
+tab_q8_3Inntekt$q8_3Inntekt<- factor(tab_q8_3Inntekt$q8_3Inntekt, levels =c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","98","99"))
+                              
+m1 <- polr(q4_10~ Alder + Kjønn  + Folkemengde + BefTetthetKommune, train, Hess=TRUE)
 summary(m1)
 
-# running model with more variables 
-#variable <- c("Folkemengde", "BefTetthetKommune","q2_6","q2_7","q2_10","Sum.felt.hjortedyr","SausluppetFylke","Tapt.saulam.fylke","Saulamtapprosent.F","Felt.HjortElg.K", "Bearzone","Wolfzone","Wolverinezone", "Lynxzone","AntallRartZone", "bjørn","gaupe","jerv","ulv","Antallarter","ArtTilstede")
-# spørsmål om en rekke variabler. 
 
 # next steps
 # p-value
