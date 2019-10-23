36d7c818d74ee87742b57ad41707fed49ee22d52
Kim Magnus Bærum <kim.barum@nina.no>
2019-10-23 18:54:39 +0200

    Update Ordinal_regression.R
    Noen komentarer

--- Ordinal_regression.R
+++ Ordinal_regression.R
@@ -1,9 +1,9 @@
 
 # Ordinal regression (proportional odds logistic regression - for multi-class ordered variables)
 
-setwd("C:/Users/magnusb/Filr/My Files/Oppgave/Data spørreundersøkelse/RETTredigert/Arbeidsfil Paper 1")
-#mydata <-read.csv("~/DIV NINA/DatasetSp�rreunders�kelseSept2019.csv",header=T,sep=";",dec = "," ,na.strings = "")
-mydata <- read.csv(file="DatasetSpørreundersøkelseOkt2019.csv",header=T,sep=";")
+#setwd("C:/Users/magnusb/Filr/My Files/Oppgave/Data spørreundersøkelse/RETTredigert/Arbeidsfil Paper 1")
+mydata <-read.csv("~/DIV NINA/DatasetSpørreundersøkelseOkt2019.csv",header=T,sep=";",dec = "," ,na.strings = "")
+#mydata <- read.csv(file="DatasetSpørreundersøkelseOkt2019.csv",header=T,sep=";")
 
 library(ggplot2)
 library(foreign)
@@ -26,6 +26,7 @@ library(tidyverse)
 
 names(mydata)
 # spørs om man bør endre navn på variabler som har norske bokstaver? Så ikke git lager kvalm med døm? i så fall må det endres i scriptet under. 
+#Kan ofte vaere en fordel aa ungaa egene norske bokstaver ja, saan generelt
 #mydata<- mydata %>% 
 #  rename(sex = Kjønn)
 # making factors and ordered factors
@@ -46,10 +47,25 @@ is.na(mydata$q3_1b)
 #variable <- c("Folkemengde", "BefTetthetKommune","q2_6","q2_7","q2_10","Sum.felt.hjortedyr","SausluppetFylke","Tapt.saulam.fylke","Saulamtapprosent.F","Felt.HjortElg.K", "Bearzone","Wolfzone","Wolverinezone", "Lynxzone","AntallRartZone", "bj�rn","gaupe","jerv","ulv","Antallarter","ArtTilstede")
 # sporsmaal om en rekke variabler. 
 
-#Det er et poeng for � validere modellen, men tror vi kj�rer all data inn i modelleringen her siden vi ikke har s� mye data i utgangspunktet. Vi m�tte is�fall ha gjort et utvalg innenfor hver kommune ja (om vi tenker at respondentene har en n�stet i kommune). 
+#Det er et poeng for å validere modellen, men tror vi kjører all data inn i modelleringen her siden vi ikke har s� mye data i utgangspunktet. Vi m�tte is�fall ha gjort et utvalg innenfor hver kommune ja (om vi tenker at respondentene har en n�stet i kommune). 
 # Det kan i s�fall enkelt gj�res ved � kj�re funksjonen i linje 37 i en loop som indekserer p� kommune.
 # Svar: Ok, kunne gjerne trengt hjelp til å sette opp dette? Aldri satt opp loop selv før. 
 # Oppgave jeg maa gjoere: Sett opp loop
+
+##KM: HMMM, tror ikke jeg husker helt hva vi skulle sette opp loop på, er det blitt borte?
+#loop kan settes opp på flere måter, men den enkleste (og tregeste hvis det er mye data) er en for-loop. Er det mye data så pleier jeg å bruke data.table-pakka, men den tar det ofte litt lengere tid å bli dus på
+
+#generelt kan en for-loop for å indeksere på en faktor settes opp sånn
+for(i in levels(faktor))
+{                                          
+  Shit that needs too be done
+  
+  eks: 
+  test.data<-subset(data, subset=variable==i)
+  cr<-with(test.data,chapmanRobson(Alder_ny_slope,lnN,1:max(test.data$Alder_ny_slope)))
+  keep.Z<-cbind(keep.Z,cr$est)
+}
+keep.Z
 
 #### The model - MASS ####
 #legger til alle variabler vi �nsker � teste
@@ -58,20 +74,28 @@ is.na(mydata$q3_1b)
 # Svar: Jeg har gjort et slags forsøk, men trenger nok hjelp til dette videre. 
 
 # Full model: - obs: får noen warnings, som jeg trenger hjelp til å ordne. "Folkemengde" fungerer f.eks. ikke.  
-#m1 <- polr(q4_10~ Alder + Kjønn  + Folkemengde + BefTetthetKommune + Sum.felt.hjortedyr + SausluppetFylke + AntallRartZone + Wolfzone + AntallRArterF + ArtTilstede + q3_1b + q2_6.5 + q2_7.5 + q2_10 , mydata, Hess=TRUE) 
-range(mydata$Sum.felt.hjortedyr)
-unique(mydata$Sum.felt.hjortedy)
-mydata$Sum.felt.hjortedy <- 
+#Dataene trengs nok å skaleres eller sentres, se hvordan det ser ut med og uten en log-transformasjon feks
 
+#m1 <- polr(q4_10~ Alder + Kjønn  + Folkemengde + BefTetthetKommune + Sum.felt.hjortedyr + SausluppetFylke + AntallRartZone + Wolfzone + AntallRArterF + ArtTilstede + q3_1b + q2_6.5 + q2_7.5 + q2_10 , mydata, Hess=TRUE) 
+hist(mydata$Folkemengde)
+hist(log(mydata$Folkemengde))
+#mydata$Sum.felt.hjortedy <- 
   
-m1 <- polr(q4_10~ Alder + Kjønn + BefTetthetKommune + Sum.felt.hjortedyr + SausluppetFylke + AntallRartZone + Wolfzone + AntallRArterF + ArtTilstede + q3_1b + q2_6.5 + q2_7.5 + q2_10 , mydata, Hess=TRUE) 
+  
+m1 <- polr(q4_10~ Alder + Kjønn +log(Folkemengde) +BefTetthetKommune + log(Sum.felt.hjortedyr) + SausluppetFylke + AntallRartZone + Wolfzone + AntallRArterF + ArtTilstede + q3_1b + q2_6.5 + q2_7.5 + q2_10 , mydata, Hess=TRUE) 
 summary(m1)
-# Spørsmål Hva velger man som vurderingsgrunnlag til å fjerne variabler? p-verdien er jo veldig liten for samtlige?  
+#Du får rank-deficient modell her fordi vi putter for mange variable inn for å forklare variasjonen. 
+
+# Spørsmål Hva velger man som vurderingsgrunnlag til å fjerne variabler? p-verdien er jo veldig liten for samtlige?
+## Svar: Det er litt forskjellige tilnærminger her, en måte er å sette opp alle mulige modeller for så å gjøre en model_average basert på disse
+#så slutt-modellen er altså en sammensettning av alle modellene, men vekte basert på AIC-verdier, og hva disse modellene inneholder. 
+#En anne måte er å sette opp en modell med alle variable vi tror gir mening, og bare oppgi denne. og en annen er igjen å finne den "enkleste" modellen basert på noen seleksjonsmetode.
+#Det man kan gjøre er å sette opp feks 10 kandidatmodeller, som vi har trua på med tanke på vraiable-kombinasjoner, og så sette disse opp mot hverandre og sjekke forskjeller i AIC-verdier
 
 # Tester variabler for å se hvilke som ikke fungerer                               
 #ma <- polr(q4_10~ Alder + Kjønn + BefTetthetKommune, mydata, Hess=TRUE) # Folkemengde fungerer ikke - får feilmelding på summary. 
 #mb <- polr(q4_10~ Alder + Kjønn + BefTetthetKommune + q8_1Utdanning + q8_3Inntekt + q8_4Parti, mydata, Hess=TRUE)
-mc <- polr(q4_10~ Alder + Kjønn + BefTetthetKommune + AntallRartZone+ Wolfzone + Antallarter + ArtTilstede, mydata, Hess=TRUE) # Inkludering av Sum.felt.hjortedyr gir warning message . In sqrt(diag(vc)) : NaNs produced
+mc <- polr(q4_10~ log(Folkemengde)+Alder + Kjønn + BefTetthetKommune + AntallRartZone+ Wolfzone + Antallarter + ArtTilstede, mydata, Hess=TRUE) # Inkludering av Sum.felt.hjortedyr gir warning message . In sqrt(diag(vc)) : NaNs produced
 #md <- polr(q4_10~ Alder + Kjønn + q3_1b + q2_6.5 + q2_7.5 + q2_10 + BefTetthetKommune + Sum.felt.hjortedyr+ AntallRartZone+ Wolfzone + Antallarter + ArtTilstede, mydata, Hess=TRUE) # Inkludering av Sum.felt.hjortedyr gir warning message  + f�r warning message design appears to be rank-deficient, In sqrt(diag(vc)) : NaNs produced
 #me <- polr(q4_10~ Alder + Kjønn + q3_1b + q2_6.5, mydata, Hess = TRUE)  # rank-deficient. 
 #mf <- polr(q4_10~ Alder + Kjønn + q2_6.5, mydata, Hess = TRUE) 
@@ -105,11 +129,6 @@ drop1(mc, type=F)
 mc2 <- polr(q4_10~ AntallRartZone+ Wolfzone + ArtTilstede, mydata, Hess=TRUE)# remove variables that are more than 2 AIC-values larger than the smallest one. 
 drop1(mc2, type=F)
 # Kommentar: Dette kan ikke være riktig måte å gjøre det på. Hva gjør egt. drop1? 
+## Nei, ikke helt stuerent, men se kommentar over. Sett opp 10-15 modeller som gir mening, og gjerne en 0modell uten variable, og test mellom disse
 
-
-
-
-#### visualising #### 
-
-# bruke Hmsic for aa sjekke proportion odds assumption? 
 
