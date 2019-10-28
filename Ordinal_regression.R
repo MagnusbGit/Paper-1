eac071d35348ae74021a84aea4585ffca71ee20d
MagnusbGit <magnus.barmoen@inn.no>
2019-10-28 16:43:47 +0100

    Trying to create training data using for loop

--- Ordinal_regression.R
+++ Ordinal_regression.R
@@ -2,7 +2,7 @@
 # Ordinal regression (proportional odds logistic regression - for multi-class ordered variables)
 
 #setwd("C:/Users/magnusb/Filr/My Files/Oppgave/Data spørreundersøkelse/RETTredigert/Arbeidsfil Paper 1")
-mydata <-read.csv("~/DIV NINA/DatasetSpørreundersøkelseOkt2019.csv",header=T,sep=";",dec = "," ,na.strings = "")
+#mydata <-read.csv("~/DIV NINA/DatasetSpørreundersøkelseOkt2019.csv",header=T,sep=";",dec = "," ,na.strings = "")
 #mydata <- read.csv(file="DatasetSpørreundersøkelseOkt2019.csv",header=T,sep=";")
 
 library(ggplot2)
@@ -11,6 +11,7 @@ library(MASS)
 library(Hmisc)
 library(reshape2)
 library(tidyverse)
+library(lattice)
 
 #str(mydata)
 #table(mydata$q4_10)
@@ -23,10 +24,7 @@ library(tidyverse)
 #  facet_grid(Kjønn ~ ArtTilstede , margins = TRUE) +
 #  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1)) # boxplot er vel uaktuelt naar man har 2110 resodenter (her tok jeg bare 100 av de for at det skulle bli leselig, og det kan man jo ikke gjore)
 
-
-names(mydata)
-# spørs om man bør endre navn på variabler som har norske bokstaver? Så ikke git lager kvalm med døm? i så fall må det endres i scriptet under. 
-#Kan ofte vaere en fordel aa ungaa egene norske bokstaver ja, saan generelt
+#### As faktor ####
 #mydata<- mydata %>% 
 #  rename(sex = Kjønn)
 # making factors and ordered factors
@@ -39,7 +37,7 @@ mydata$q2_10 <- as.ordered(mydata$q2_10)
 mydata$AntallRartZone <- as.factor(mydata$AntallRartZone)
 mydata$AntallRArterF <- as.factor(mydata$AntallRArterF) # Endret til hva som så ut til å være en gruppert versjon av variablen?
 #mydata$Antallarter <- as.factor(mydata$Antallarter)
-mydata$q3_1b[mydata$q3_1b==4] <- NA # does this work? 
+mydata$q3_1b[mydata$q3_1b==4] <- NA  
 is.na(mydata$q3_1b)
 
 # Question: do this with all variables of interest. Maybe extracting interesting variables first? 
@@ -47,6 +45,8 @@ is.na(mydata$q3_1b)
 #variable <- c("Folkemengde", "BefTetthetKommune","q2_6","q2_7","q2_10","Sum.felt.hjortedyr","SausluppetFylke","Tapt.saulam.fylke","Saulamtapprosent.F","Felt.HjortElg.K", "Bearzone","Wolfzone","Wolverinezone", "Lynxzone","AntallRartZone", "bj�rn","gaupe","jerv","ulv","Antallarter","ArtTilstede")
 # sporsmaal om en rekke variabler. 
 
+
+#### Train data ####
 #Det er et poeng for å validere modellen, men tror vi kjører all data inn i modelleringen her siden vi ikke har s� mye data i utgangspunktet. Vi m�tte is�fall ha gjort et utvalg innenfor hver kommune ja (om vi tenker at respondentene har en n�stet i kommune). 
 # Det kan i s�fall enkelt gj�res ved � kj�re funksjonen i linje 37 i en loop som indekserer p� kommune.
 # Svar: Ok, kunne gjerne trengt hjelp til å sette opp dette? Aldri satt opp loop selv før. 
@@ -61,26 +61,43 @@ for(i in levels(faktor))
   Shit that needs too be done
   
   eks: 
-  test.data<-subset(data, subset=variable==i)
+  test.data<-subset(data, subset=Value==i)
   cr<-with(test.data,chapmanRobson(Alder_ny_slope,lnN,1:max(test.data$Alder_ny_slope)))
   keep.Z<-cbind(keep.Z,cr$est)
 }
 keep.Z
 
-#### The model - MASS ####
+# Man kan gjøre slik for å hente ut to rader fra samme kommuner: 
+for(i in levels(mydata$Kommune))
+{                                          
+  test.data <- mydata %>% group_by(Kommune) %>% sample_n(size = 2)
+}
+# skal man ha inn en test inn i denne, slik som under? (bortsett fra at jeg ikke helt forstod hvordan dette skulle gjoeres så den er helt feil saa langt)
+# men tror jeg trenger hjelp til aa forstaa hva vi oensker aa oppnaa
+for(i in levels(mydata$Kommune))
+{                                          
+  test.data <- mydata %>% group_by(Kommune) %>% sample_n(size = 2)
+  cr<-with(test.data, polr(q4_10,1:max(test.data$q4_10)))
+  keep.Z<-cbind(keep.Z,cr$est)
+}
+keep.Z
+# ser ogsaa at det er mange forskjellige maater aa gjoere det paa.
+# .. som LOOCV og k-fold. Er det den sistnevte vi gjoer bare at vi passer paa aa alltid ha med f.eks. 3 av samme kommune i trainingsettet og 2 i test?
+
+mydata2
+test.data
+str(test.data)
+str(mydata)
+#### MASS ####
 #legger til alle variabler vi �nsker � teste
 
-# Tenker det er greit å bruke AIC som modelseleksjonskriterie her, kjenner du til gangen i det, eller skal jeg sette opp for deg?
-# Svar: Jeg har gjort et slags forsøk, men trenger nok hjelp til dette videre. 
-
-# Full model: - obs: får noen warnings, som jeg trenger hjelp til å ordne. "Folkemengde" fungerer f.eks. ikke.  
 #Dataene trengs nok å skaleres eller sentres, se hvordan det ser ut med og uten en log-transformasjon feks
-
 #m1 <- polr(q4_10~ Alder + Kjønn  + Folkemengde + BefTetthetKommune + Sum.felt.hjortedyr + SausluppetFylke + AntallRartZone + Wolfzone + AntallRArterF + ArtTilstede + q3_1b + q2_6.5 + q2_7.5 + q2_10 , mydata, Hess=TRUE) 
 hist(mydata$Folkemengde)
 hist(log(mydata$Folkemengde))
 #mydata$Sum.felt.hjortedy <- 
-  
+hist(mydata$Sum.felt.hjortedyr)
+hist(log(mydata$Sum.felt.hjortedyr)) # er det mulig aa log-transforemre her, eller maa noe annet gjoeres?   
   
 m1 <- polr(q4_10~ Alder + Kjønn +log(Folkemengde) +BefTetthetKommune + log(Sum.felt.hjortedyr) + SausluppetFylke + AntallRartZone + Wolfzone + AntallRArterF + ArtTilstede + q3_1b + q2_6.5 + q2_7.5 + q2_10 , mydata, Hess=TRUE) 
 summary(m1)
