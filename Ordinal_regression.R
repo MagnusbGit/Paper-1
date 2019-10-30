4702d8c65ee32508e808c13823ab6ad4f6d66170
Magnus Barmoen <magnus.barmoen@hihm.no>
2019-10-30 18:13:16 +0100

    Coming up with alternative models

--- Ordinal_regression.R
+++ Ordinal_regression.R
@@ -12,6 +12,8 @@ library(Hmisc)
 library(reshape2)
 library(tidyverse)
 library(lattice)
+library(Rtools)
+library(bbmle)
 
 #str(mydata)
 #table(mydata$q4_10)
@@ -37,22 +39,20 @@ mydata$q2_10 <- as.ordered(mydata$q2_10)
 mydata$AntallRartZone <- as.factor(mydata$AntallRartZone)
 mydata$AntallRArterF <- as.factor(mydata$AntallRArterF) # Endret til hva som så ut til å være en gruppert versjon av variablen?
 #mydata$Antallarter <- as.factor(mydata$Antallarter)
-mydata$q3_1b[mydata$q3_1b==4] <- NA  
-is.na(mydata$q3_1b)
 
+# TO DO: Grab q3_1- and q3_2averanges from OrdinalPaper1-script.  
+names(mydata)
 
 #### Train data ####
 
-#MB: Man kan vel gjoere slik for aa hente ut to rader fra samme kommuner: 
-##KM Ja, dette er egentlig en bedre og foretrukket måte for en såpass "enkel" oppgave (da uten å ha det i en loop), men var mer for å vise hvordan man kan sette det opp i en loop også
+#Hente ut to rader fra samme kommuner: 
 
 #for(i in levels(mydata$Kommune))
 #{                                          
-  test.data <- mydata %>% group_by(Kommune) %>% sample_n(size = 2)
+test.data <- mydata %>% group_by(Kommune) %>% sample_n(size = 2)
 #}
-#MB: skal man ha inn en test inn i denne, slik som under? (bortsett fra at jeg ikke helt forstod hvordan dette skulle gjoeres så den er helt feil saa langt)
-#MB: men tror jeg trenger hjelp til aa forstaa hva vi oensker aa oppnaa
-##KM: Det man kan gjøre er å lage modellen med teningsdata, og så bruke den modellen for å predikere for den andre delen (testdata). På den måten kan man se feks hvor generell modellen er,
+
+## TO DO: lag modellen med teningsdata, og så bruke den modellen for å predikere for den andre delen (testdata). På den måten kan man se feks hvor generell modellen er,
 ##dvs hvor godt treffer den predikerte verdien for testdata i forhold til faktisk/målte verdier i testdata?
 
 #for(i in levels(mydata$Kommune))
@@ -66,8 +66,6 @@ is.na(mydata$q3_1b)
 # MB: .. som LOOCV og k-fold. Er det den sistnevte vi gjoer bare at vi passer paa aa alltid ha med f.eks. 3 av samme kommune i trainingsettet og 2 i test?
 ## KM: Jepp, alltid mange måter å gjøre alt på ;-), men poenget er at man på en elelr annen måte får testet modellen sin. Om det da er et poeng at den skal være generell.
 ## Om den er ment for å være en mer beskrivende modell, så bruker man bare all data man har inn i å trene opp modellen, og beskriver resultatene utifra det.
-mydata2
-test.data
 str(test.data)
 str(mydata)
 
@@ -87,17 +85,14 @@ m1 <- polr(q4_10~ Alder + Kjønn +log(Folkemengde) +BefTetthetKommune + log(Sum.
 summary(m1)
 #Du får rank-deficient modell her fordi vi putter for mange variable inn for å forklare variasjonen. 
 
-#MB: Spørsmål Hva velger man som vurderingsgrunnlag til å fjerne variabler? p-verdien er jo veldig liten for samtlige?
-## Svar: Det er litt forskjellige tilnærminger her, en måte er å sette opp alle mulige modeller for så å gjøre en model_average basert på disse
+## Svar: Det er litt forskjellige tilnærminger til å redusere variabler, en måte er å sette opp alle mulige modeller for så å gjøre en model_average basert på disse
 #så slutt-modellen er altså en sammensettning av alle modellene, men vekte basert på AIC-verdier, og hva disse modellene inneholder. 
 #En anne måte er å sette opp en modell med alle variable vi tror gir mening, og bare oppgi denne. og en annen er igjen å finne den "enkleste" modellen basert på noen seleksjonsmetode.
 #Det man kan gjøre er å sette opp feks 10 kandidatmodeller, som vi har trua på med tanke på vraiable-kombinasjoner, og så sette disse opp mot hverandre og sjekke forskjeller i AIC-verdier
 #MB: Jeg tenkte foerst vi burde proeve aa selektere basert paa kandidatmodeller. Men er det ikke en del kritikk ift bruk av stepwise selection ogsaa? 
 ## KM: mener her å sette opp modeller basert på antagelser fra litteratur/eller annen vitenskap. Så ikke selektere per variabel egentlig, men sette opp hele modeller man har trua på, og så teste disse i mellom seg.
 ## Har man feks kunnskap, eller en ide om at Kjønn og Alder uansett er viktig her, så er det ikke noe vits å teste ut dette på nytt
-
-# MB: er det noe problem aa gjoere begge varianter som du nevner? 
-## KM: Ikke noe problem, men du bør bare presentere en av dem
+# kan gjøre begge varianter, men bare presenter en av dem. 
 
 
 
@@ -105,7 +100,7 @@ summary(m1)
 # Modellseleksjon AIC #
 #######################
 #setter opp et eksempel med bakgrunn  i variablene som er med i modellene under (og med en grunnløs (?) antagelse om at kjønn og alder har alltid noe å si)
-
+names(mydata)
 # Kunn effekt av demografi (dvs ingen effekt av variable)
 
 m0 <- polr(q4_10~ Alder + Kjønn, mydata, Hess=TRUE)
@@ -113,23 +108,61 @@ m0 <- polr(q4_10~ Alder + Kjønn, mydata, Hess=TRUE)
 ### Effekt av tilsetdeværelse av rovdyrarter 
 
 m1 <- polr(q4_10~ Alder + Kjønn + ArtTilstede, mydata, Hess=TRUE)
+ 
+m1a <- polr(q4_10~ Alder + Kjønn + ArtTilstede, mydata, Hess=TRUE)
+m1b <- polr(q4_10~ Alder + Kjønn + q3_1average, mydata, Hess=TRUE)
+m1c <- polr(q4_10~ Alder + Kjønn + q3_2average, mydata, Hess=TRUE)
+# Effekt av tilsetdeværelse av rovdyrzone
+m1d <- polr(q4_10~ Alder + Kjønn + ArtTilstede + RzoneTilstede, mydata, Hess=TRUE)
+m1e <- polr(q4_10~ Alder + Kjønn+ ArtTilstede + Wolfzone, mydata, Hess=TRUE)
 
 # Antagligvis så vil effekten av artilsetdeværelse variere med alder (bare ment hypotetisk)
 
 m2 <- polr(q4_10~ Alder*ArtTilstede + Kjønn , mydata, Hess=TRUE)
-
+m2b <- polr(q4_10~ Alder*q3_1average + kjønn, mydata, Hess=TRUE)
 ### Sosioøkonomiske effekter
 
 m3 <- polr(q4_10~ Alder + Kjønn + ArtTilstede+q8_1Utdanning+q8_3Inntekt, mydata, Hess=TRUE)
+
+### Sosioøkonomiske og demografi
+m4 <- polr(q4_10~ Alder + Kjønn + log(Folkemengde) + BefTetthetKommune + q8_1Utdanning + q8_4Parti, mydata, Hess =T)
+
+### Rovdyr tilstede
+
+m5 <- polr(q4_10~ Alder + Kjønn + ArtTilstede+q8_1Utdanning+q8_3Inntekt, mydata, Hess=TRUE)
+
+### Attitude rovdyr
+
+m6 <- polr(q4_10~ Alder + Kjønn + ArtTilstede + q8_1Utdanning+q8_3Inntekt, mydata, Hess=TRUE)
+
+### Jakttradisjoner 
+
+m7a <- polr(q4_10~ Alder + Kjønn + ArtTilstede + q2_10 + q2_11.1 + q2_11.3 , mydata, Hess=TRUE)
+
+m7b <- polr(q4_10~ log(Sum.felt.hjortedyr) + q2_10 + q2_11.1 + q2_6.5 + q2_7.5 + q2_11.1, mydata, Hess =T) # Kan kanskje droppe denne som bare er en kombinasjon av to? For det dekkes uansett, eller?  
+
+### attitude and experience damage + hunting
+
+m8 <- polr(q4_10~ Alder + Kjønn + ArtTilstede + q2_1.5 + q2_2.5 + q2_3.5 + q2_6.5 + q2_7.5 + log(Sum.felt.hjortedyr), mydata, Hess=TRUE)
+
+### Tillit 
+m9 <- polr(q4_10~ q4_1 + q4_2+q4_3+q4_5+q4_6+q4_7+q4_8+q4_9, mydata, Hess =T) # SPM: Basert paa faktoranalysen kan vi vel ikke slaa sammen noen av disse variablene, evt. velge ut noen (i saa fall maa vel valget baseres paa hvilkne vi har mest tro paa basert på oekologi eller lignende? 
+
+### Modell jeg har god tro paa paa tvers av temaer
+m10 <- polr(q4_10~Alder+Kjønn+ArtTilstede+q2_6.5+q3_1average+q6_average,mydata,Hess=T) # 
+#tilpasning ved å slette de man har minst tro på (ved aa jukse og se paa)
+m11 <- polr(q4_10~Alder+ArtTilstede+q2_6.5+q3_1average+q6_average,mydata,Hess=T) # Kjoenn boer bort
+m12 <- polr(q4_10~Alder+q2_6.5+q3_1average+q6_average,mydata,Hess=T)
+m13 <- polr(q4_10~Alder+ArtTilstede+q2_6.5+q3_1average,mydata,Hess=T)
 
 ##OSV, osv
 
 # Se på forskjeller mellom modeller
 bbmle::ICtab(m0,m1,m2,m3, type="AICc", logLik = T)
-
+bbmle::ICtab(m0,m1,m2,m3,m10,m11,m12,m13, type="AICc", logLik = T)
 
 ## Lager tabeller
-stargazer::stargazer(m2, m3,type="html",
+stargazer::stargazer(m2, m10,type="html",
                      title="Regression Results",
                      intercept.bottom = F,
                      intercept.top = T,
@@ -140,22 +173,20 @@ stargazer::stargazer(m2, m3,type="html",
                      out="C:/Users/magnusb/Filr/My Files/Oppgave/Data spørreundersøkelse/RETTredigert/Arbeidsfil Paper 1/modeller.doc")##mange flere muligheter på layout her, sjekk ?stargazer
 
 
+
 #### Alternative modeller ####                             
 # SPM: Boer vi gruppere dem inn i temaer? Eller boer jeg velge ut den modellen jeg har best tro paa paa tvers av "tema"? 
 #m1 <- polr(q4_10~ Alder + Kjønn + q3_1b + q2_6.5 + q2_7.5 + q2_10 + BefTetthetKommune + Sum.felt.hjortedyr+ AntallRartZone+ Wolfzone + Antallarter + ArtTilstede, mydata, Hess=TRUE) # Inkludering av Sum.felt.hjortedyr gir warning message  + f�r warning message design appears to be rank-deficient, In sqrt(diag(vc)) : NaNs produced
-names(mydata)
 # hva skjer om jeg gjoer q4
-mSE <-polr(q4_10~ Alder + Kjønn + log(Folkemengde) + BefTetthetKommune + q8_1Utdanning + q8_2Arbeid + q8_3Inntekt + q8_4Parti, mydata, Hess =T)
-mR <- polr(q4_10~ Bearzone + Wolfzone + Lynxzone + Wolverinezone + AntallRartZone +RzoneTilstede+bjørn+gaupe+jerv+ulv+Antallarter+ArtTilstede, mydata, Hess =T) # DROPP VARIABLER
-mByland <- polr(q4_10~ Alder + Kjønn + log(Folkemengde) + BefTetthetKommune + q8_1Utdanning + q8_2Arbeid + q8_3Inntekt + q8_4Parti, mydata, Hess =T)
-mJ <- polr(q4_10~ log(Sum.felt.hjortedyr) + q2_10 + q2_11.1, mydata, Hess =T) # Jegertradisjoner
-mD <- polr(q4_10~+ q2_6.5 + q2_7.5 + q2_11.1, mydata, Hess =T) # J# Damage . OBS- SPM: Hva blir riktig å inkludere for q2_6 og q2_7, se spoerreundersoekelse
-mJandD <- polr(q4_10~ log(Sum.felt.hjortedyr) + q2_10 + q2_11.1,q2_6.5 + q2_7.5 + q2_11.1, mydata, Hess =T) # Kan kanskje droppe denne som bare er en kombinasjon av to? For det dekkes uansett, eller?  
-mAtt <- polr(factor(q4_10) ~ q3_1median + q3_2median + q6_average, mydata,Hess = T) # NB need to run OversiktPaper1 q3_2median before running this
-mExp #model experience 
-mTrust <- polr(q4_10~ q4_1 + q4_2+q4_3+q4_5+q4_6+q4_7+q4_8+q4_9, mydata, Hess =T) # SPM: Basert paa faktoranalysen kan vi vel ikke slaa sammen noen av disse variablene, evt. velge ut noen (i saa fall maa vel valget baseres paa hvilkne vi har mest tro paa basert på oekologi eller lignende? 
-
-mkomb <- # 
+#mSE <-polr(q4_10~ Alder + Kjønn + log(Folkemengde) + BefTetthetKommune + q8_1Utdanning + q8_2Arbeid + q8_3Inntekt + q8_4Parti, mydata, Hess =T)
+#mR <- polr(q4_10~ Bearzone + Wolfzone + Lynxzone + Wolverinezone + AntallRartZone +RzoneTilstede+bjørn+gaupe+jerv+ulv+Antallarter+ArtTilstede, mydata, Hess =T) # DROPP VARIABLER
+#mByland <- polr(q4_10~ Alder + Kjønn + log(Folkemengde) + BefTetthetKommune + q8_1Utdanning + q8_2Arbeid + q8_3Inntekt + q8_4Parti, mydata, Hess =T)
+#mJ <- polr(q4_10~ log(Sum.felt.hjortedyr) + q2_10 + q2_11.1, mydata, Hess =T) # Jegertradisjoner
+#mD <- polr(q4_10~+ q2_6.5 + q2_7.5 + q2_11.1, mydata, Hess =T) # J# Damage . OBS- SPM: Hva blir riktig å inkludere for q2_6 og q2_7, se spoerreundersoekelse
+#mJandD <- polr(q4_10~ log(Sum.felt.hjortedyr) + q2_10 + q2_11.1,q2_6.5 + q2_7.5 + q2_11.1, mydata, Hess =T) # Kan kanskje droppe denne som bare er en kombinasjon av to? For det dekkes uansett, eller?  
+#mAtt <- polr(factor(q4_10) ~ q3_1median + q3_2median + q6_average, mydata,Hess = T) # NB need to run OversiktPaper1 q3_2median before running this
+#mExp #model experience 
+#mTrust <- polr(q4_10~ q4_1 + q4_2+q4_3+q4_5+q4_6+q4_7+q4_8+q4_9, mydata, Hess =T) # SPM: Basert paa faktoranalysen kan vi vel ikke slaa sammen noen av disse variablene, evt. velge ut noen (i saa fall maa vel valget baseres paa hvilkne vi har mest tro paa basert på oekologi eller lignende? 
 
 # div jeg ikke har tatt stilling til ennaa 
 #ma <- polr(q4_10~ Alder + Kjønn + BefTetthetKommune, mydata, Hess=TRUE) # Folkemengde fungerer ikke - får feilmelding på summary. 
