a8783c36fa08e0b562d1416b16ecd91508b53338
Magnus Barmoen <magnus.barmoen@hihm.no>
2019-12-02 10:19:50 +0100

    Set up the file showing a more clean oversight of the model-fitting process

---
+++ Modelseleksjon.R
@@ -0,0 +1,84 @@
+X<-mydata[,c("q4_1", "q4_2","q4_3","q4_4","q4_5","q4_6","q4_7","q4_8","q4_9")]
+library(GGally)
+ggpairs(X)
+
+# Gjør til faktor
+mydata$q4_1 <- as.factor(mydata$q4_1)
+mydata$q4_2 <- as.factor(mydata$q4_2)
+mydata$q4_3 <- as.factor(mydata$q4_3)
+mydata$q4_4 <- as.factor(mydata$q4_4)
+mydata$q4_5 <- as.factor(mydata$q4_5)
+mydata$q4_6 <- as.factor(mydata$q4_6)
+mydata$q4_7 <- as.factor(mydata$q4_7)
+mydata$q4_8 <- as.factor(mydata$q4_8) # ikke med
+mydata$q4_9 <- as.factor(mydata$q4_9) # ikke med 
+mydata$q4_10 <- as.factor(mydata$q4_10) 
+
+# Test tillit forskning - forklare tillit til rovviltforskning relatert til tillt forskere og forskning
+# Forklaring på variablene
+# 1= forskning er viktig
+# 2=alvorlig mister tro på forskning
+# 3=tillit generell forskning
+# 4 = tillit medisinsk forsk
+# 5 = Tillit klimaforskning
+# 6 = Forskere har høy ekspertise
+# 7 = forskere har høyr troverdighet
+m9 <- polr(q4_10~ 1,mydata, Hess =T)  # Best
+m9a <- polr(q4_10~ q4_3,mydata, Hess =T) 
+m9b <- polr(q4_10~ q4_4,mydata, Hess =T)
+m9c <- polr(q4_10~ q4_5,mydata, Hess =T)
+bbmle::ICtab(m9,m9a,m9b,m9c, type="AICc", logLik = T) # 
+
+m9d <- polr(q4_10~ q4_3 + q4_1, mydata, Hess =T)
+m9e <- polr(q4_10~ q4_3 + q4_2, mydata, Hess =T) # Bedre 
+bbmle::ICtab(m9a,m9d,m9e, type="AICc", logLik = T) 
+
+m9f <- polr(q4_10~ q4_3 + q4_2 + q4_6, mydata, Hess =T)
+m9g <- polr(q4_10~ q4_3 + q4_2 + q4_7, mydata, Hess =T) # Bedre
+bbmle::ICtab(m9e,m9f,m9g, type="AICc", logLik = T) 
+
+### tilstedeværelse
+#"RzoneTilstede"
+#"ArtTilstede" 
+m2a <- polr(q4_10~ q4_3 + q4_2 + q4_7 + ArtTilstede, mydata, Hess =T) # bedre
+m2b <- polr(q4_10~ q4_3 + q4_2 + q4_7 + RzoneTilstede, mydata, Hess =T)
+bbmle::ICtab(m9e,m2a,m2b, type="AICc", logLik = T) 
+
+### attitude
+# Holdning: (mydata$q3_1average) - TODO Kom tilbake til denne - DEN MÅ FIKSES OM DEN SKAL BRUKES
+# m3 <- polr(q4_10~ q4_3 + q4_2 + q4_7 + ArtTilstede +mydata$q3_1average , mydata, Hess =T) # best
+
+#m3 <- polr(q4_10~ q4_3 + q4_2 + q4_7 + ArtTilstede + q3_3_3 , mydata, Hess =T) 
+
+m3a <- polr(q4_10~ q4_3 + q4_2 + q4_7 + ArtTilstede + q3_3_3 , mydata, Hess =T) 
+m3b <- polr(q4_10~ q4_3 + q4_2 + q4_7 + ArtTilstede + q3_3_4 , mydata, Hess =T) 
+m3c <- polr(q4_10~ q4_3 + q4_2 + q4_7 + ArtTilstede + q3_3_5 , mydata, Hess =T) # bedre
+m3d <- polr(q4_10~ q4_3 + q4_2 + q4_7 + ArtTilstede + q3_3_6 , mydata, Hess =T) 
+m3e <- polr(q4_10~ q4_3 + q4_2 + q4_7 + ArtTilstede + q3_3_7 , mydata, Hess =T) 
+m3f <- polr(q4_10~ q4_3 + q4_2 + q4_7 + ArtTilstede + q3_3_8 , mydata, Hess =T) 
+bbmle::ICtab(m9g,m3a,m3b,m3c, m3d,m3e,m3f, type="AICc", logLik = T) 
+
+### Sos Demografi
+m4a <- polr(q4_10~ q4_3 + q4_2 + q4_7 + ArtTilstede + q3_3_8 + Alder, mydata, Hess =T) 
+m4b <- polr(q4_10~ q4_3 + q4_2 + q4_7 + ArtTilstede + q3_3_8 + Kjønn, mydata, Hess =T)
+m4c <- polr(q4_10~ q4_3 + q4_2 + q4_7 + ArtTilstede + q3_3_8 + q8_1Utdanning, mydata, Hess =T) # bedre
+bbmle::ICtab(m3c,m4b,m4c, type="AICc", logLik = T) 
+
+m4d <- polr(q4_10~ q4_3 + q4_2 + q4_7 + ArtTilstede + q3_3_8 + q8_1Utdanning + Alder, mydata, Hess =T) # Bedre
+bbmle::ICtab(m4c,m4d, type="AICc", logLik = T) 
+
+### Jakttradisjoner  
+m5a <- polr(q4_10~ q4_3 + q4_2 + q4_7 + ArtTilstede + q3_3_8 + q8_1Utdanning + Alder + q2_10 , mydata, Hess =T)
+m5b <- polr(q4_10~ q4_3 + q4_2 + q4_7 + ArtTilstede + q3_3_8 + q8_1Utdanning + Alder + q2_11.1 , mydata, Hess =T) # Bedre
+bbmle::ICtab(m4d,m5a,m5b, type="AICc", logLik = T) 
+
+
+# QUESTION: modellen blir bedre, men kan vi inkludere q2_11.1 som den er?
+
+### attitude and experience damage + hunting
+m6a <- polr(q4_10~ q4_3 + q4_2 + q4_7 + ArtTilstede + q3_3_8 + q8_1Utdanning + Alder + + q2_11.1 + q2_6.5, mydata, Hess =T)
+m6b <- polr(q4_10~ q4_3 + q4_2 + q4_7 + ArtTilstede + q3_3_8 + q8_1Utdanning + Alder + + q2_11.1 + q2_7.5, mydata, Hess =T) # Bedre
+bbmle::ICtab(m5b,m6a,m6b, type="AICc", logLik = T) 
+# QUESTION: modellen blir bedre, men kan vi inkludere q2_7.5?
+
+
